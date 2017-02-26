b659333fd84d1369d692bf98d324fbf16fb06725
Aarthyravi <aarthydravi@gmail.com>
2017-02-25 17:37:32 -0800

    Add files via upload

--- models/blogmodel.py
+++ models/blogmodel.py
@@ -1,1 +1,15 @@
+from google.appengine.ext import db
+from models.commentmodel import Comment
+# Blog Model
+class Blog(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    lastmodi = db.DateTimeProperty(auto_now = True)
+    user = db.StringProperty(required = True)
+    likes = db.IntegerProperty(required=True)
+    liked_by = db.ListProperty(str)
 
+    @property
+    def comments(self):
+        return Comment.all().filter("post = ", str(self.key().id()))
---
+++ models/commentmodel.py
@@ -0,0 +1,9 @@
+from google.appengine.ext import db
+
+# Comment Model
+class Comment(db.Model):
+    comment = db.StringProperty(required=True)
+    post = db.StringProperty(required=True)
+    author = db.StringProperty(required=True)
+
+    
