6d0bc3e5df07b0ea61352f9e08301d97cdd8b7fb
Aarthyravi <aarthydravi@gmail.com>
2017-02-28 13:06:28 -0800

    Update signup.py

--- handlers/signup.py
+++ handlers/signup.py
@@ -1,7 +1,11 @@
 from models.usermodel import User
 from mainhandler import Handler
 import mainhandler
-# Signup Handler with username,password,Verify password and optional email and check the Validation for all field.
+# Signup Handler with username,password,
+# Verify password and optional email and
+# check the Validation for all field.
+
+
 class SignupHandler(Handler):
     def get(self):
         self.render("signup.html")
@@ -13,29 +17,33 @@ class SignupHandler(Handler):
         verify = self.request.get("verify")
         email = self.request.get("email")
 
-        param = dict(username = username,password = password,email=email)
+        param = dict(username=username, password=password, email=email)
         if not valid_username(username):
-             param['error_username']="This is not a valid username"
-             error = True
+            param['error_username'] = "This is not a valid username"
+            error = True
         if not valid_password(password):
-            param['error_password']="This is not a valid password"
+            param['error_password'] = "This is not a valid password"
             error = True
         elif password != verify:
-            param['error_verify']="Your password didn't match"
+            param['error_verify'] = "Your password didn't match"
             error = True
         if not valid_email(email):
-            param['error_email']="This is not a valid email"
+            param['error_email'] = "This is not a valid email"
             error = True
         if error:
-            self.render("signup.html",**param)
+            self.render("signup.html", **param)
         else:
             u = User.by_name(username)
             if u:
                 msg = 'This username already exists!'
-                self.render('signup.html', error_username = msg)
+                self.render('signup.html', error_username=msg)
             else:
-                u = User(name=username, pw_hash=make_pw_hash(username, password), email=email)
+                u = User(name=username,
+                         pw_hash=make_pw_hash(username, password),
+                         email=email)
                 u.put()
                 new_cookie = make_secure(str(username))
-                self.response.headers.add_header('Set-Cookie', 'user=%s; Path=/' % new_cookie)
+                self.response.headers.add_header('Set-Cookie',
+                                                 'user=%s; Path=/'
+                                                 % new_cookie)
                 self.redirect('/blog/welcome')
