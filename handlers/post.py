5e58790dc43a588a02f048d2b979afcec5b97907
Aarthyravi <aarthydravi@gmail.com>
2017-02-28 13:02:15 -0800

    Update post.py

--- handlers/post.py
+++ handlers/post.py
@@ -1,14 +1,16 @@
 from mainhandler import Handler
 from models.blogmodel import Blog
 from google.appengine.ext import db
-
 import mainhandler
 # Single post display with comments
+
+
 class BlogPost(Handler):
-     def get(self,post_id):
-         key = db.Key.from_path('Blog',int(post_id))
-         post = db.get(key)
-         if not post:
+    def get(self, post_id):
+        key = db.Key.from_path('Blog', int(post_id))
+        post = db.get(key)
+        if not post:
             self.error(404)
             return
-         self.render("post1.html",post=post)
+        self.render("post1.html", post=post)
+
