b5f7a9dfeb17553ef92915f774e21e1bd687dcb3
Aarthyravi <aarthydravi@gmail.com>
2017-02-28 11:10:37 -0800

    Update likepost.py

--- handlers/likepost.py
+++ handlers/likepost.py
@@ -1,4 +1,4 @@
-from handlers.mainhandler import Handler
+from mainhandler import Handler
 from models.blogmodel import Blog
 from google.appengine.ext import db
 # User of the post can not be able to like their post. only other user like
