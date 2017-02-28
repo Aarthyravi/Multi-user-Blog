cbd0faf4ea3088c44b4590952bc243842ec2dda7
Aarthyravi <aarthydravi@gmail.com>
2017-02-28 12:28:58 -0800

    Update blogmodel.py

--- models/blogmodel.py
+++ models/blogmodel.py
@@ -1,12 +1,14 @@
 from google.appengine.ext import db
 from models.commentmodel import Comment
 # Blog Model
+
+
 class Blog(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
-    lastmodi = db.DateTimeProperty(auto_now = True)
-    user = db.StringProperty(required = True)
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    lastmodi = db.DateTimeProperty(auto_now=True)
+    user = db.StringProperty(required=True)
     likes = db.IntegerProperty(required=True)
     liked_by = db.ListProperty(str)
 
