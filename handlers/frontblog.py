adc18e5ca2f5192e32d1a09f4ff51478500bdb8e
Aarthyravi <aarthydravi@gmail.com>
2017-02-28 12:51:16 -0800

    Update frontblog.py

--- handlers/frontblog.py
+++ handlers/frontblog.py
@@ -2,7 +2,10 @@ from mainhandler import Handler
 from models.blogmodel import Blog
 from google.appengine.ext import db
 # Front page of Blog
+
+
 class MainPage(Handler):
     def get(self):
-        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC limit 10")
-        self.render("front.html",blogs=blogs)
+        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY
+                            created DESC limit 10")
+        self.render("front.html", blogs=blogs)
