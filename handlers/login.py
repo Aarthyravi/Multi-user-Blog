5a1bd912029f43b65427b30d98891dc723d6b474
Aarthyravi <aarthydravi@gmail.com>
2017-02-28 12:55:00 -0800

    Update login.py

--- handlers/login.py
+++ handlers/login.py
@@ -2,19 +2,22 @@ from models.usermodel import User
 from mainhandler import Handler
 import mainhandler
 # Login Handler with username & Password
+
+
 class LoginHandler(Handler):
     def get(self):
-        self.render('login.html',error=self.request.get('error'))
+        self.render('login.html', error=self.request.get('error'))
 
     def post(self):
         username = self.request.get('username')
         password = self.request.get('password')
 
-        u = User.login(username,password)
+        u = User.login(username, password)
         if u:
             new_cookie = make_secure(str(username))
-            self.response.headers.add_header('Set-Cookie', 'user=%s; Path=/' % new_cookie)
+            self.response.headers.add_header('Set-Cookie',
+                                             'user=%s; Path=/' % new_cookie)
             self.redirect('/blog/welcome')
         else:
             msg = 'Invalid login'
-            self.render('login.html', error = msg)
+            self.render('login.html', error=msg)
